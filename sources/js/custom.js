a57d117d30f7f1e9fdd747d5824b8b0295510a4a
Muhamad Nauval Azhar <nauvalazhar2@gmail.com>
2019-02-20 02:38:19 +0700

    revert files

--- sources/js/custom.js
+++
@@ -1,8 +0,0 @@
-/**
- *
- * You can write your JS code here, DO NOT touch the default style file
- * because it will make it harder for you to update.
- * 
- */
-
-"use strict";
--- sources/js/page/modules-ion-icons.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$("#icons li").each(function() {
-  $(this).append('<div class="icon-name">'+ $(this).attr('class') +'</div>');
-});
-$("#icons li").click(function() {
-  $(".icon-name").fadeOut();
-  $(this).find('.icon-name').fadeIn();
-});
