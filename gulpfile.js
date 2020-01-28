97b2ce829c167357a9e43127e16895b7eeec077e
Oscar Cervantes <baltadlakd@gmail.com>
2020-01-28 17:25:02 -0600

    pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,113 @@
 /**
  * Imports
  */
-const {src, dest, watch, parallel} = require('gulp');
-const notify = require('gulp-notify');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const plumber = require('gulp-plumber');
-const rename = require('gulp-rename');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const uglify = require('gulp-uglify-es').default;
-const imagemin = require('gulp-imagemin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const nunjucks = require('gulp-nunjucks');
-const color = require('gulp-color');
-const nodePath = require('path');
+const { src, dest, watch, parallel } = require("gulp");
+const notify = require("gulp-notify");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const uglify = require("gulp-uglify-es").default;
+const imagemin = require("gulp-imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const nunjucks = require("gulp-nunjucks");
+const color = require("gulp-color");
+const nodePath = require("path");
 
 /**
  * Configuration
  * @type {String}
  */
-var cssDir = 'assets/css',
-    jsDir = 'assets/js',
-    htmlDir = 'sources/pages',
-    sassDir = 'sources/scss',
-    imgDir = 'assets/img';
+var cssDir = "assets/css",
+  jsDir = "assets/js",
+  htmlDir = "sources/pages",
+  sassDir = "sources/scss",
+  imgDir = "assets/img";
 
 /**
  * Helpers
  */
 
-function _compile_html(path, onEnd, log=true, ret=false) {
-  if(log)
-    _log('[HTML] Compiling: ' + path, 'GREEN');
+function _compile_html(path, onEnd, log = true, ret = false) {
+  if (log) _log("[HTML] Compiling: " + path, "GREEN");
 
   let compile_html = src(path, { base: htmlDir })
-  .pipe(plumber())
-  .pipe(nunjucks.compile({
-    version: '2.3.0',
-    site_name: 'Stisla'
-  },
-  /**
-   * Nunjucks options
-   */
-  {
-    trimBlocks: true,
-    lstripBlocks: true,
-    /**
-     * Nunjucks filters
-     * @type {Object}
-     */
-    filters: {
-      is_active: (str, reg, page) => {
-        reg = new RegExp(reg, 'gm');
-        reg = reg.exec(page);
-        if(reg != null) {
-          return str;
+    .pipe(plumber())
+    .pipe(
+      nunjucks.compile(
+        {
+          version: "2.3.0",
+          site_name: "Stisla"
+        },
+        /**
+         * Nunjucks options
+         */
+        {
+          trimBlocks: true,
+          lstripBlocks: true,
+          /**
+           * Nunjucks filters
+           * @type {Object}
+           */
+          filters: {
+            is_active: (str, reg, page) => {
+              reg = new RegExp(reg, "gm");
+              reg = reg.exec(page);
+              if (reg != null) {
+                return str;
+              }
+            }
+          }
         }
-      }
-    }
-  }))
-  .on('error', console.error.bind(console))
-  .on('end', () => {
-    if(onEnd)
-      onEnd.call(this);
+      )
+    )
+    .on("error", console.error.bind(console))
+    .on("end", () => {
+      if (onEnd) onEnd.call(this);
 
-    if(log)
-      _log('[HTML] Finished', 'GREEN');
-  })
-  .pipe(dest('pages'))
-  .pipe(plumber.stop());
+      if (log) _log("[HTML] Finished", "GREEN");
+    })
+    .pipe(dest("pages"))
+    .pipe(plumber.stop());
 
-  if(ret) return compile_html;
+  if (ret) return compile_html;
 }
 
-function _compile_scss(path, onEnd, log=true, ret=false) {
-  if(log)
-    _log('[SCSS] Compiling:' + path, 'GREEN');
+function _compile_scss(path, onEnd, log = true, ret = false) {
+  if (log) _log("[SCSS] Compiling:" + path, "GREEN");
 
   let compile_scss = src(path)
-  .pipe(plumber())
-  .pipe(sass({
-    errorLogToConsole: true
-  }))
-  .on('error', console.error.bind(console))
-  .on('end', () => {
-    if(onEnd)
-      onEnd.call(this);
+    .pipe(plumber())
+    .pipe(
+      sass({
+        errorLogToConsole: true
+      })
+    )
+    .on("error", console.error.bind(console))
+    .on("end", () => {
+      if (onEnd) onEnd.call(this);
 
-    if(log)
-      _log('[SCSS] Finished', 'GREEN');
-  })
-  .pipe(rename({
-    dirname: '',
-    extname: '.css'
-  }))
-  .pipe(postcss([autoprefixer()]))
-  .pipe(dest(cssDir))
-  .pipe(plumber.stop());
+      if (log) _log("[SCSS] Finished", "GREEN");
+    })
+    .pipe(
+      rename({
+        dirname: "",
+        extname: ".css"
+      })
+    )
+    .pipe(postcss([autoprefixer()]))
+    .pipe(dest(cssDir))
+    .pipe(plumber.stop());
 
-  if(ret) return compile_scss;
+  if (ret) return compile_scss;
 }
 
 function _log(str, clr) {
-  if(!clr) clr = 'WHITE';
+  if (!clr) clr = "WHITE";
   console.log(color(str, clr));
 }
 
@@ -118,29 +120,27 @@ function _log(str, clr) {
  */
 
 function folder() {
-  return src('*.*', {read: false})
-  .pipe(dest('./assets'))
-  .pipe(dest('./assets/css'))
-  .pipe(dest('./assets/js'))
-  .pipe(dest('./assets/img'));
+  return src("*.*", { read: false })
+    .pipe(dest("./assets"))
+    .pipe(dest("./assets/css"))
+    .pipe(dest("./assets/js"))
+    .pipe(dest("./assets/img"));
 }
 
 function image() {
-  return src(imgDir + '/**/*.*')
-  .pipe(plumber())
-  .pipe(imagemin([
-    imageminMozjpeg({quality: 80})
-  ]))
-  .pipe(dest(imgDir))
-  .pipe(plumber.stop());
+  return src(imgDir + "/**/*.*")
+    .pipe(plumber())
+    .pipe(imagemin([imageminMozjpeg({ quality: 80 })]))
+    .pipe(dest(imgDir))
+    .pipe(plumber.stop());
 }
 
 function compile_scss() {
-  return _compile_scss(sassDir + '/**/*.scss', null, false, true);
+  return _compile_scss(sassDir + "/**/*.scss", null, false, true);
 }
 
 function compile_html() {
-  return _compile_html(htmlDir + '/**/*.html', null, false, true);
+  return _compile_html(htmlDir + "/**/*.html", null, false, true);
 }
 
 function watching() {
@@ -152,41 +152,41 @@ function watching() {
    * @type {Object}
    */
   browserSync.init({
-    server:{
+    server: {
       baseDir: "./"
     },
-    startPath: 'pages/index.html',
-    port: 8080
+    startPath: "pages/index.html",
+    port: 8081
   });
 
   /**
    * Watch ${htmlDir}
    */
   watch([
-    htmlDir + '/**/*.html',
-    sassDir + '/**/*.scss',
-    jsDir + '/**/*.js',
-    imgDir + '/**/*.*',
-  ]).on('change', (file) => {
+    htmlDir + "/**/*.html",
+    sassDir + "/**/*.scss",
+    jsDir + "/**/*.js",
+    imgDir + "/**/*.*"
+  ]).on("change", file => {
     file = file.replace(/\\/g, nodePath.sep);
 
-    if(file.indexOf('.scss') > -1) {
-      _compile_scss(sassDir + '/**/*.scss', () => {
+    if (file.indexOf(".scss") > -1) {
+      _compile_scss(sassDir + "/**/*.scss", () => {
         return browserSync.reload();
       });
     }
 
-    if(file.indexOf('layouts') > -1 && file.indexOf('.html') > -1) {
-      _compile_html(htmlDir + '/*.html', () => {
+    if (file.indexOf("layouts") > -1 && file.indexOf(".html") > -1) {
+      _compile_html(htmlDir + "/*.html", () => {
         return browserSync.reload();
       });
-    }else if(file.indexOf('.html') > -1) {
+    } else if (file.indexOf(".html") > -1) {
       _compile_html(file, () => {
         return browserSync.reload();
       });
     }
 
-    if(file.indexOf(jsDir) > -1 || file.indexOf(imgDir) > -1) {
+    if (file.indexOf(jsDir) > -1 || file.indexOf(imgDir) > -1) {
       return browserSync.reload();
     }
   });
